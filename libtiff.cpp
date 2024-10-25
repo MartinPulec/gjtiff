ddea79c50ea1974443c5145a1b5317f2788b429a
Martin Pulec <martin.pulec@cesnet.cz>
2024-10-25 10:45:27 +0200

    libtiff: decode tiled directly
    files like
    S1A_IW_GRDH_1SDV_20240722T050955_20240722T051020_054869_06AEB3_BA8A_COG.SAFE/measurement/s1a-iw-grd-vv-20240722t050955-20240722t051020-054869-06aeb3-001-cog.tif
    use ZSTD-compressed tiles with 16u samples with values typically 0-400,
    which is not possible to process with TIFFReadRGBAImage, because the
    values will be then just 0s and 1s.

--- libtiff.cpp
+++ libtiff.cpp
@@ -12,6 +12,9 @@
 #include "kernels.hpp"
 #include "utils.hpp"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 void nullTIFFErrorHandler(const char *, const char *, va_list) {}
 
 struct libtiff_state {
@@ -32,6 +35,7 @@ struct libtiff_state {
 
         struct dec_image decode_fallback(TIFF *tif);
         struct dec_image decode_stripped_complex(TIFF *tif, struct tiff_info *tiffinfo);
+        struct dec_image decode_tiled(TIFF *tif, struct tiff_info *tiffinfo);
 };
 
 libtiff_state::libtiff_state(int l, cudaStream_t s) : log_level(l), stream(s)
@@ -145,6 +149,74 @@ libtiff_state::decode_stripped_complex(TIFF *tif, struct tiff_info *tiffinfo)
         return ret;
 }
 
+struct dec_image
+libtiff_state::decode_tiled(TIFF *tif, struct tiff_info *tiffinfo)
+{
+        if (tiffinfo->sample_format != SAMPLEFORMAT_UINT ||
+            tiffinfo->big_endian || tiffinfo->common.comp_count > 1 ||
+            tiffinfo->bits_per_sample != 16) {
+                return {};
+        }
+
+        struct dec_image ret = tiffinfo->common;
+
+        assert(tiffinfo->strip_tile_size > 0);
+        const tstrip_t numberOfTiles = TIFFNumberOfTiles(tif);
+        size_t decsize = tiffinfo->strip_tile_size * numberOfTiles;
+        if (decsize > decoded_allocated) {
+                CHECK_CUDA(cudaFreeHost(decoded));
+                CHECK_CUDA(cudaFree(d_decoded));
+                CHECK_CUDA(cudaMallocHost(&decoded, decsize));
+                CHECK_CUDA(cudaMalloc(&d_decoded, decsize));
+                decoded_allocated = decsize;
+        }
+
+        const int tile_count_vertical =
+            (tiffinfo->common.width + tiffinfo->tile_width - 1) /
+            tiffinfo->tile_width;
+        const int bps = tiffinfo->bits_per_sample / 8;
+        const int dst_linesize = tiffinfo->common.width * bps;
+        const int tile_linesize = tiffinfo->tile_width * bps;
+        TIMER_START(TIFFReadEncodedTile, log_level);
+        for (tstrip_t tile = 0; tile < numberOfTiles; ++tile) {
+                size_t offset = tile * tiffinfo->strip_tile_size;
+                uint8_t *tile_data = decoded + offset;
+                const tmsize_t rc = TIFFReadEncodedTile(
+                    tif, tile, tile_data, tiffinfo->strip_tile_size);
+                if (rc == -1) {
+                        ERROR_MSG("libtiff decode image %s failed!\n",
+                                  TIFFFileName(tif));
+                        return {};
+                }
+                size_t offset_x =
+                    (tile % tile_count_vertical) * tiffinfo->tile_width;
+                size_t offset_y =
+                    (tile / tile_count_vertical) * tiffinfo->tile_height;
+                size_t dst_offset = offset_y * dst_linesize + offset_x * bps;
+                const int rows =
+                    MIN(tiffinfo->tile_height, ret.height - offset_y);
+                const int columns =
+                    MIN(tiffinfo->tile_width, ret.width - offset_x);
+                CHECK_CUDA(cudaMemcpy2DAsync(
+                    d_decoded + dst_offset, dst_linesize, decoded + offset,
+                    tile_linesize, (size_t)columns * bps, rows,
+                    cudaMemcpyHostToDevice, stream));
+        }
+        TIMER_STOP(TIFFReadEncodedTile, log_level);
+        const size_t converted_size =
+            (size_t) ret.height * dst_linesize * ret.comp_count;
+        if (converted_size > d_converted_allocated) {
+                CHECK_CUDA(cudaFree(d_converted));
+                CHECK_CUDA(cudaMalloc(&d_converted, converted_size));
+                d_converted_allocated = converted_size;
+        }
+        // convert_complex_int(d_decoded, d_converted, decsize, stream);
+        ret.data = d_decoded;
+        convert_16_8_cuda(&ret, d_converted, stream);
+        ret.data = d_converted;
+        return ret;
+}
+
 struct dec_image libtiff_state::decode(const char *fname)
 {
         TIFF *tif = TIFFOpen(fname, "r");
@@ -158,10 +230,9 @@ struct dec_image libtiff_state::decode(const char *fname)
                 print_tiff_info(tiffinfo);
         }
 
-        struct dec_image ret{};
-        if (!tiffinfo.tiled) {
-                ret = decode_stripped_complex(tif, &tiffinfo);
-        }
+        struct dec_image ret = tiffinfo.tiled
+                                   ? decode_tiled(tif, &tiffinfo)
+                                   : decode_stripped_complex(tif, &tiffinfo);
 
         if (ret.data == nullptr) {
                 ret = decode_fallback(tif);
