15e1a30a3e2555e26a8ec3193d7024fca514ad35
Martin Pulec <martin.pulec@cesnet.cz>
2024-11-05 11:19:32 +0100

    libnvj2k: convert interleaved
    the nvjpeg2000 library actually can decoded interleaved

--- libnvj2k.cpp
+++ libnvj2k.cpp
@@ -20,6 +20,7 @@ enum {
 struct nvj2k_state {
         nvjpeg2kHandle_t nvjpeg2k_handle;
         nvjpeg2kStream_t nvjpeg2k_stream;
+        nvjpeg2kDecodeParams_t decode_params;
         nvjpeg2kDecodeState_t decode_state;
 
         cudaStream_t cuda_stream;
@@ -27,21 +28,26 @@ struct nvj2k_state {
         char *bitstream_buffer;
         size_t bitstream_buffer_allocated;
 
-        uint8_t *decode_output[MAX_COMPONENTS];
+        uint8_t *decode_output;
         unsigned decode_output_width,
             decode_output_height;
-        size_t pitch_in_bytes[MAX_COMPONENTS];
+        size_t pitch_in_bytes;
 
         uint8_t *converted;
         size_t converted_allocated;
 };
 
+/**
+ * @todo error handling
+ */
 struct nvj2k_state *nvj2k_init(cudaStream_t stream) {
         struct nvj2k_state *s = (struct nvj2k_state *) calloc(1, sizeof *s);
 
         nvjpeg2kCreateSimple(&s->nvjpeg2k_handle);
         nvjpeg2kDecodeStateCreate(s->nvjpeg2k_handle, &s->decode_state);
         nvjpeg2kStreamCreate(&s->nvjpeg2k_stream);
+        nvjpeg2kDecodeParamsCreate(&s->decode_params);
+        nvjpeg2kDecodeParamsSetOutputFormat(s->decode_params, NVJPEG2K_FORMAT_INTERLEAVED);
         s->cuda_stream = stream;
         return s;
 }
@@ -99,16 +105,17 @@ struct dec_image nvj2k_decode(struct nvj2k_state *s, const char *fname) {
         }
 
         nvjpeg2kImage_t output_image;
+        output_image.pixel_type = NVJPEG2K_UINT8;
+        output_image.num_components = image_info.num_components;
 
+        const int bps = 1;
         if (s->decode_output_width != image_comp_info[0].component_width ||
             s->decode_output_height != image_comp_info[0].component_height) {
-                for (int i = 0; i < image_info.num_components; i++) {
-                        cudaFree(s->decode_output[i]);
-                        cudaMallocPitch((void **)&s->decode_output[i],
-                                        &s->pitch_in_bytes[i],
-                                        image_comp_info[0].component_width,
-                                        image_comp_info[0].component_height);
-                }
+                cudaFree(s->decode_output);
+                cudaMallocPitch((void **)&s->decode_output, &s->pitch_in_bytes,
+                                (size_t)image_comp_info[0].component_width *
+                                    bps * image_info.num_components,
+                                image_comp_info[0].component_height);
         }
         // check that all comps has the same size
         for (int c = 1; c < image_info.num_components; c++) {
@@ -129,15 +136,13 @@ struct dec_image nvj2k_decode(struct nvj2k_state *s, const char *fname) {
         s->decode_output_width = image_comp_info[0].component_width;
         s->decode_output_height = image_comp_info[0].component_height;
 
-        output_image.pixel_data = (void **) s->decode_output;
-        output_image.pixel_type = NVJPEG2K_UINT8;
-        output_image.pitch_in_bytes = s->pitch_in_bytes;
-        output_image.num_components = image_info.num_components;
+        output_image.pixel_data = (void **) &s->decode_output;
+        output_image.pitch_in_bytes = &s->pitch_in_bytes;
 
         TIMER_START(nvjpeg2kDecode, log_level);
-        status =
-            nvjpeg2kDecode(s->nvjpeg2k_handle, s->decode_state,
-                           s->nvjpeg2k_stream, &output_image, s->cuda_stream);
+        status = nvjpeg2kDecodeImage(s->nvjpeg2k_handle, s->decode_state,
+                                     s->nvjpeg2k_stream, s->decode_params,
+                                     &output_image, s->cuda_stream);
         TIMER_STOP(nvjpeg2kDecode, log_level);
         if (status != NVJPEG2K_STATUS_SUCCESS) {
                 ERROR_MSG("Unable to decode J2K file %s: %d\n", fname,
@@ -146,29 +151,19 @@ struct dec_image nvj2k_decode(struct nvj2k_state *s, const char *fname) {
         }
 
         size_t conv_size = (size_t)s->decode_output_width *
-                          s->decode_output_height * image_info.num_components;
+                           s->decode_output_height * bps *
+                           image_info.num_components;
         if (s->converted_allocated < conv_size) {
                 cudaFree(s->converted);
                 cudaMalloc((void **)&s->converted, conv_size);
                 s->converted_allocated = conv_size;
         }
 
-        switch (image_info.num_components) {
-        case 1:
-                convert_grayscale_remove_pitch(
-                    s->decode_output[0], s->converted, s->decode_output_width,
-                    s->pitch_in_bytes[0], s->decode_output_height,
-                    s->cuda_stream);
-        case 3:
-                convert_planar_rgb_to_packed(
-                    s->decode_output, s->converted, s->decode_output_width,
-                    s->pitch_in_bytes[0], s->decode_output_height,
-                    s->cuda_stream);
-        default:
-                ERROR_MSG("%s: unsupported component count %d!\n", fname,
-                          (int)image_info.num_components);
-                return {};
-        }
+        convert_grayscale_remove_pitch(
+            s->decode_output, s->converted,
+            (int)(s->decode_output_width * image_info.num_components),
+            (int)s->pitch_in_bytes, (int)s->decode_output_height,
+            s->cuda_stream);
 
         const struct dec_image ret = {(enum rc)0, (int)s->decode_output_width,
                                       (int)s->decode_output_height,
@@ -179,10 +174,9 @@ struct dec_image nvj2k_decode(struct nvj2k_state *s, const char *fname) {
 
 void nvj2k_destroy(struct nvj2k_state *s) {
         cudaFreeHost(s->bitstream_buffer);
-        for (int c = 0; c < MAX_COMPONENTS; c++) {
-                cudaFree(s->decode_output[c]);
-        }
+        cudaFree(s->decode_output);
         cudaFree(s->converted);
+        nvjpeg2kDecodeParamsDestroy(s->decode_params);
         nvjpeg2kStreamDestroy(s->nvjpeg2k_stream);
         nvjpeg2kDecodeStateDestroy(s->decode_state);
         nvjpeg2kDestroy(s->nvjpeg2k_handle);
