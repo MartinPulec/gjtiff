d84b3045e94aad6df999205f18e7296fdca433c0
Martin Pulec <martin.pulec@cesnet.cz>
2025-01-31 14:46:25 +0100

    rotate: handle lon +179->-179 transition
    eastern (left) to western hemisphere
    
    (tested on Vanua Levu picture)

--- rotate.c
+++ rotate.c
@@ -48,12 +48,18 @@ void rotate_destroy(struct rotate_state *s)
  */
 static double normalize_coords(struct coordinate coords[4])
 {
-        double lat_min = 90.0;
-        double lat_max = -90.0;
-        double lon_min = 180.0;
-        double lon_max = -180.0;
+        double lat_min = 1e6;
+        double lat_max = -1e6;
+        double lon_min = 1e6;
+        double lon_max = -1e6;
 
         for (unsigned i = 0; i < 4; ++i) {
+                // make both positive:
+                // - lat - 0-180
+                // - lon - 0-360
+                coords[i].latitude += 90;
+                coords[i].longitude += 180;
+
                 if (coords[i].latitude < lat_min) {
                         lat_min = coords[i].latitude;
                 }
@@ -66,6 +72,17 @@ static double normalize_coords(struct coordinate coords[4])
                 if (coords[i].longitude > lon_max) {
                         lon_max = coords[i].longitude;
                 }
+        }
+
+        // handle longitude 179->-179 transition (eastern to western
+        // hemishpere); zero is now shifted to 180
+        if (lon_min < 180 && lon_max >= 180) {
+                for (unsigned i = 0; i < 4; ++i) {
+                        if (coords[i].longitude < 180) {
+                                coords[i].longitude += 360.0;
+                        }
+                }
+                return normalize_coords(coords);
         }
 
         double lat_range = lat_max - lat_min;
