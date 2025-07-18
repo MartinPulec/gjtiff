ab461a251491ce43e164f823929f8e89f89afd99
Martin Pulec <martin.pulec@cesnet.cz>
2025-07-18 11:30:32 +0200

    WIP rotate_utm: compute dst coords

--- rotate_utm.cu
+++ rotate_utm.cu
@@ -58,95 +58,6 @@ void rotate_utm_destroy(struct rotate_utm_state *s)
         free(s);
 }
 
-/**
- * normalize the coordinates to 0..1 and
- * @return asoect ratio
- */
-static double normalize_coords(const struct coordinate src_coords[4],
-                               struct coordinate coords[4])
-{
-        double lat_min = 0;
-        double lat_max = 0;
-        double lon_min = 0;
-        double lon_max = 0;
-
-        // check if not near poles
-        get_lat_lon_min_max(src_coords, &lat_min, &lat_max, &lon_min, &lon_max);
-        if (lat_min < -85. || lat_max > 85.) {
-                const double near_pole_pt_lat = (lat_max > 85 ? lat_max
-                                                              : lat_min);
-                WARN_MSG("Not normalizing areas near North/South Pole! (at "
-                         "least one point at most 5° /%f°/ degrees from the "
-                         "Pole)\n",
-                         near_pole_pt_lat);
-                return -1;
-        }
-
-        for (unsigned i = 0; i < 4; ++i) {
-                double lat_rad = src_coords[i].latitude / 180. * M_PI;
-                coords[i].latitude = (M_PI -
-                                      log(tan((M_PI / 4.) + (lat_rad / 2.)))) /
-                                     (2. * M_PI);
-                double lon_rad = src_coords[i].longitude / 180. * M_PI;
-                coords[i].longitude = (M_PI + lon_rad) / (2. * M_PI);
-        }
-
-        get_lat_lon_min_max(coords, &lat_min, &lat_max, &lon_min, &lon_max);
-
-        double lat_range = lat_max - lat_min;
-        double lon_range = lon_max - lon_min;
-
-        // fprintf(stderr,
-        //         "lat_min: %f lat_max: %f lon_min: %f lon_max: %f lat_range: %f "
-        //         "lon_range: %f\n",
-        //         lat_min, lat_max, lon_min, lon_max, lat_range, lon_range);
-
-        for (unsigned i = 0; i < 4; ++i) {
-                coords[i].latitude = (coords[i].latitude - lat_min) / lat_range;
-                coords[i].longitude = (coords[i].longitude - lon_min) /
-                                      lon_range;
-        }
-
-        return lon_range / lat_range;
-}
-
-/// fullfill GPUJPEG mem requirements
-static void adjust_size(int *width, int *height, int comp_count) {
-
-        enum {
-                GB1 = 1LL * 1000 * 1000 * 1000,
-                GJ_PER_BYTE_REQ = 20,
-        };
-        ssize_t threshold = mem_limit;
-        if (threshold == 0) {
-                threshold = MIN((ssize_t)gpu_memory / 2,
-                                (ssize_t)gpu_memory - 2 * GB1);
-                assert(threshold >= (ssize_t)2 * GB1);
-        }
-        ssize_t gj_gram_needed = (ssize_t)*width * *height * comp_count *
-                                 GJ_PER_BYTE_REQ;
-        if (gj_gram_needed < threshold) {
-                return;
-        }
-        WARN_MSG(
-            "[rotate] Encoding of %dx%d image would require %.2f GB GRAM (>=%g "
-            "GB), downsizing ",
-            *width, *height, (double)gj_gram_needed / GB1,
-            (double)threshold / GB1);
-        while (gj_gram_needed > threshold) {
-                *width /= 2;
-                *height /= 2;
-                gj_gram_needed /= 4;
-        }
-        WARN_MSG("rotated to %.2f GB (%dx%d).\n", (double)gj_gram_needed / GB1,
-                 *width, *height);
-}
-
-static void release_owned_image(struct owned_image *img) {
-        CHECK_CUDA(cudaFree(img->img.data));
-        free(img);
-}
-
 // Device function: bilinear sample at (x, y) in [0..W) × [0..H)
 __device__ uint8_t bilinearSample(
     const uint8_t* src,
@@ -159,11 +70,11 @@ __device__ uint8_t bilinearSample(
     int x1 = x0 + 1;
     int y1 = y0 + 1;
 
-    // Clamp to image edges
-    x0 = max(0, min(x0, W - 1));
-    y0 = max(0, min(y0, H - 1));
-    x1 = max(0, min(x1, W - 1));
-    y1 = max(0, min(y1, H - 1));
+    // // Clamp to image edges - doesn't happen
+    // x0 = max(0, min(x0, W - 1));
+    // y0 = max(0, min(y0, H - 1));
+    // x1 = max(0, min(x1, W - 1));
+    // y1 = max(0, min(y1, H - 1));
 
     // Fetch four neighbors
     float I00 = src[y0 * W + x0];
@@ -228,11 +139,11 @@ static __global__ void kernel_to_wgs84(device_projection const d_proj,
                 return;
         }
 
-        if (out_x == out_width / 2 && out_y == out_height / 2) {
-                printf("%f %f\n\n", pos_wgs84.x, pos_wgs84.y);
-                printf("%f %f\n\n", pos_utm.x, pos_utm.y);
-                printf("%f %f\n\n", rel_pos_src_x, rel_pos_src_y);
-        }
+        // if (out_x == out_width / 2 && out_y == out_height / 2) {
+                // printf("%f %f\n\n", pos_wgs84.x, pos_wgs84.y);
+                // printf("%f %f\n\n", pos_utm.x, pos_utm.y);
+                // printf("%f %f\n\n", rel_pos_src_x, rel_pos_src_y);
+        // }
 
         double abs_pos_src_x = rel_pos_src_x * in_width;
         double abs_pos_src_y = rel_pos_src_y * in_height;
@@ -278,6 +189,10 @@ static struct owned_image *to_epsg_4326(struct rotate_utm_state *s,
                 dst_desc.height = (int)(in->width / dst_ratio);
         }
         struct owned_image *ret = new_cuda_owned_image(&dst_desc);
+        for (unsigned i = 0; i < ARR_SIZE(ret->img.bounds); ++i) {
+                ret->img.bounds[i] = dst_bounds.bound[i];
+        }
+        snprintf(ret->img.authority, sizeof ret->img.authority, "%s", "EPSG:4326");
 
         using coordinate = cuproj::vec_2d<float>;
         auto proj = cuproj::make_projection<coordinate>( "EPSG:4326", in->authority );
@@ -293,75 +208,47 @@ static struct owned_image *to_epsg_4326(struct rotate_utm_state *s,
         return ret;
 }
 
+// WSG84 (lon, lat) to Web Mercator
+static struct owned_image *
+epsg_4326_to_epsg_3857(const struct dec_image *in,
+                       int orig_width, int orig_height)
+{
+        const double src_ratio = (double) orig_width / orig_height;
+
+        double lat_rad_top = in->bounds[YMAX] / 180. * M_PI;
+        double lat_rad_bottom = in->bounds[YMIN] / 180. * M_PI;
+        double lat_merc_top = (M_PI -
+                               log(tan((M_PI / 4.) + (lat_rad_top / 2.)))) /
+                              (2. * M_PI);
+        double lat_merc_bottom = (M_PI - log(tan((M_PI / 4.) +
+                                                 (lat_rad_bottom / 2.)))) /
+                                 (2. * M_PI);
+        double dst_height = lat_merc_top - lat_merc_bottom;
+        double lon_rad_left = in->bounds[XMIN] / 180. * M_PI;
+        double lon_rad_right = in->bounds[XMAX] / 180. * M_PI;
+        double lon_merc_left  = (M_PI + lon_rad_left) / (2. * M_PI);
+        double lon_merc_right  = (M_PI + lon_rad_right) / (2. * M_PI);
+        double dst_width = lon_merc_left - lon_merc_right;
+        double dst_ratio = dst_width / dst_height;
+
+        struct dec_image dst_desc = *in;
+        if (dst_ratio >= src_ratio) {
+                dst_desc.width = (int)(in->height * dst_ratio);
+        } else {
+                dst_desc.height = (int)(in->width / dst_ratio);
+        }
+        struct owned_image *ret = new_cuda_owned_image(&dst_desc);
+
+        return ret;
+}
+
 struct owned_image *rotate_utm(struct rotate_utm_state *s, const struct dec_image *in)
 {
         struct owned_image *epsg4326 = to_epsg_4326(s, in);
         if (epsg4326 == nullptr) {
                 return nullptr;
         }
-        return epsg4326;
-
-        double aSrcQuad[4][2] = {
-            {0.0, 0.0},                              // Top-left
-            {(double)in->width, 0},                  // Top-right
-            {(double)in->width, (double)in->height}, // Bottom-right
-            {0.0, (double)in->height}                // Bottom-left
-        };
-
-        struct coordinate coords[4];
-        const double dst_aspect = normalize_coords(in->coords, coords);
-
-        NppiRect oSrcROI = {0, 0, in->width, in->height};
-        NppiSize oSrcSize = {in->width, in->height};
-
-        struct owned_image *ret = (struct owned_image *) malloc(sizeof *ret);
-        memcpy(&ret->img, in, sizeof *in);
-        ret->free = release_owned_image;
-        // keep one side as in original and upscale the other to meet dst
-        // projection dimension
-        const double src_aspect = (double)in->width / in->height;
-        if (dst_aspect >= src_aspect) {
-                ret->img.width = (int)(ret->img.height * dst_aspect);
-        } else {
-                ret->img.height = (int)(ret->img.width / dst_aspect);
-        }
-        adjust_size(&ret->img.width, &ret->img.height, in->comp_count);
-
-        const size_t req_size = (size_t)ret->img.width * ret->img.height *
-                                ret->img.comp_count;
-        CHECK_CUDA(cudaMalloc((void **)&ret->img.data, req_size));
-
-        NppiRect oDstROI = {0, 0, ret->img.width, ret->img.height};
-        double aDstQuad[4][2] = {
-            {coords[0].longitude * ret->img.width,
-             coords[0].latitude * ret->img.height}, // Top-left
-            {coords[1].longitude * ret->img.width,
-             coords[1].latitude * ret->img.height}, // Top-right
-            {coords[2].longitude * ret->img.width,
-             coords[2].latitude * ret->img.height}, // Bottom-right
-            {coords[3].longitude * ret->img.width,
-             coords[3].latitude * ret->img.height}, // Bottom-left
-        };
-
-        GPU_TIMER_START(rotate, LL_DEBUG, s->stream);
-        CHECK_CUDA(cudaMemsetAsync(ret->img.data, 0,
-                                   (size_t)ret->img.width * ret->img.height *
-                                       ret->img.comp_count,
-                                   s->stream));
-        const int interpolation = NPPI_INTER_LINEAR;
-        if (in->comp_count == 1) {
-                CHECK_NPP(nppiWarpPerspectiveQuad_8u_C1R_Ctx(
-                    in->data, oSrcSize, in->width, oSrcROI, aSrcQuad,
-                    ret->img.data, ret->img.width, oDstROI, aDstQuad,
-                    interpolation, s->nppStreamCtx));
-        } else {
-                assert(in->comp_count == 3);
-                CHECK_NPP(nppiWarpPerspectiveQuad_8u_C3R_Ctx(
-                    in->data, oSrcSize, 3 * in->width, oSrcROI, aSrcQuad,
-                    ret->img.data, 3 * ret->img.width, oDstROI, aDstQuad,
-                    interpolation, s->nppStreamCtx));
-        }
-        GPU_TIMER_STOP(rotate);
-
-        return ret;
+        struct owned_image *epsg3857 = epsg_4326_to_epsg_3857(&epsg4326->img, in->width, in->height);
+        epsg4326->free(epsg4326);
+        return epsg3857;
 }
